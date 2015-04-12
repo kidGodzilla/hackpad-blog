cb5a46d21f1589f5644e039eb668e45057245110
James Futhey <futhey@gmail.com>
2015-04-12 11:16:40 -0700

    handle hashchange events

--- js/main.js
+++ js/main.js
@@ -25,13 +25,22 @@ $(function(){
         });
     }
 
-    var hash = window.location.hash?window.location.hash:'';
-    hash = hash.replace("#", "");
+    function handleHashchange () {
+        var hash = window.location.hash?window.location.hash:'';
+        hash = hash.replace("#", "");
 
-    if (hash === '') hash = window.homepage_hash || '';
-    $('body').removeClass('homepage');
-    loadHackpad(hash);
+        if (hash === '') hash = window.homepage_hash || '';
+        $('body').removeClass('homepage');
+        loadHackpad(hash);
+    }
 
+    // Init
+    handleHashchange();
+
+    // Handle hashchange events
+    $(window).hashchange(handleHashchange);
+
+    // Hamburger menu
     $('.hamburger').click(function(){
         var $header = $('header');
         if($header.hasClass('mobile-menu-open')){
@@ -40,44 +49,6 @@ $(function(){
             $header.addClass('mobile-menu-open');
         }
     });
-
-    //var $inputinput = $('.input input');
-    //
-    //$inputinput.focus(function() {
-    //   var inputvalue = $(this).val();
-    //   // console.log(inputvalue);
-    //   if (inputvalue == '') {
-    //    $(this).parents('.linkbox').addClass('focused');
-    //   }
-    //});
-    //
-    //$inputinput.blur(function() {
-    //   var inputvalue = $(this).val();
-    //   if (inputvalue == '') {
-    //    $(this).parents('.linkbox').removeClass('focused');
-    //   }
-    //});
-
-    //$('.button').click(function(e){
-    //    e.preventDefault();
-    //    var inputvalue = $('.input input').val();
-    //    if (inputvalue == '') {
-    //        load_url = 'http://wentin.github.io/Hack-the-Hackpad/#' + 'dK2se3bCFHo';
-    //        window.location.href = load_url;
-    //        window.location.reload();
-    //    } else {
-    //        load_url = 'http://wentin.github.io/Hack-the-Hackpad/#' + inputvalue;
-    //        window.location.href = load_url;
-    //        window.location.reload();
-    //    }
-    //});
-
-    //$('.question').click(function(e){
-    //    e.preventDefault();
-    //    load_url = 'http://wentin.github.io/Hack-the-Hackpad/#' + 'JVgfJHOyu6O';
-    //    window.location.href = load_url;
-    //    window.location.reload();
-    //})
 
 });
 
